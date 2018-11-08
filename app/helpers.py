5df31e3b20b0a56337234cea044778d028b24b14
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2018-11-08 09:33:52 -0800

    properly handle deauthed users

--- app/helpers.py
+++ app/helpers.py
@@ -2,9 +2,12 @@ from ohapi import api
 
 
 def get_download_url(oh_member):
-    files = api.exchange_oauth2_member(
-        access_token=oh_member.get_access_token())['data']
-    for f in files:
-        if f['basename'] == 'oura-data.json':
-            return {'url': f['download_url'], 'created': f['created']}
-    return None
+    try:
+        files = api.exchange_oauth2_member(
+            access_token=oh_member.get_access_token())['data']
+        for f in files:
+            if f['basename'] == 'oura-data.json':
+                return {'url': f['download_url'], 'created': f['created']}
+        return None
+    except:
+        return 'token_broken'
